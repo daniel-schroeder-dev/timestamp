59e92919c6bfee8eefa783a95bfc88efefafb3e0
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-11 14:26:01 -0500

    Create test-stub for timestamp route.

---
+++ test/timestamp.js
@@ -0,0 +1,10 @@
+const assert = require('assert').strict;
+
+describe('Timestamp api', function() {
+  
+  describe('GET /api/timestamp/:valid_date_string', function() {
+    it('should return JSON with unix and utc fields');
+  });
+
+
+});
